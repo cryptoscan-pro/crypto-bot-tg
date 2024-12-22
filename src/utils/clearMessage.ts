4442e24a0bef57a9715acfa910f3372c0e345807
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-22 23:03:09 +0700

    fix(clearMessage.ts): remove unnecessary replacements in clearMessage function to simplify the code and improve readability

--- src/utils/clearMessage.ts
+++ src/utils/clearMessage.ts
@@ -1,5 +1,5 @@
 import telegramify from "telegramify-markdown";
 
 export const clearMessage = (text: string) => {
-  return telegramify(text).replaceAll('**', '§§').replaceAll('*', '').replaceAll('§§', '**');
+  return telegramify(text);
 }
